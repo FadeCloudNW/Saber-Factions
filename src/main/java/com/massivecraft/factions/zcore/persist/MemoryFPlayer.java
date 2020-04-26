dd5ef833be2f9033b2dcfe86868bd0d2d955b19a
Aleksey Titov <admin>
2020-04-25 23:19:05 -0400

    annoying /leave msg removed

--- src/main/java/com/massivecraft/factions/zcore/persist/MemoryFPlayer.java
+++ src/main/java/com/massivecraft/factions/zcore/persist/MemoryFPlayer.java
@@ -755,10 +755,6 @@ public abstract class MemoryFPlayer implements FPlayer {
 
         if (myFaction.isNormal() && !perm && myFaction.getFPlayers().isEmpty()) {
             // Remove this faction
-            for (FPlayer fplayer : FPlayers.getInstance().getOnlinePlayers())
-                fplayer.msg(TL.LEAVE_DISBANDED, myFaction.describeTo(fplayer, true));
-
-
             FactionDisbandEvent disbandEvent = new FactionDisbandEvent(getPlayer(), myFaction.getId(), PlayerDisbandReason.LEAVE);
             Bukkit.getPluginManager().callEvent(disbandEvent);
 
